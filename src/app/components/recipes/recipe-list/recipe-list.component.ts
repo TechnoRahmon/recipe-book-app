f570c4943ad92ac083c01d357a188dc2b8450417
Technorahmon <Abdulrahmanmuhialdeen@hotmail.com>
2023-07-25 13:05:50 +0300

    recipe model + maping recipes with ngFor

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Array<Recipe> = [
+    new Recipe(
+      'Garlic and lemon prawn pasta recipe',
+      `With garlic butter prawns, vibrant veg and a hint of lemon, it only takes 5 ingredients and 15 minutes to throw together this fresh and fancy pasta. It is the ideal dish to whip up for any unexpected guests!`,
+      'https://img.taste.com.au/mOx3fOxf/w720-h480-cfill-q80/taste/2022/09/garlic-chilli-prawn-pasta-181440-1.jpg'
+      ),
+      new Recipe(
+        'Garlic and lemon prawn pasta recipe',
+        `With garlic butter prawns, vibrant veg and a hint of lemon, it only takes 5 ingredients and 15 minutes to throw together this fresh and fancy pasta. It is the ideal dish to whip up for any unexpected guests!`,
+        'https://img.taste.com.au/mOx3fOxf/w720-h480-cfill-q80/taste/2022/09/garlic-chilli-prawn-pasta-181440-1.jpg'
+        )
+  ];
 
 }
